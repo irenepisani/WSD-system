b772ac2fdd009b387a92d934c6ac0bd55ed707cc
irenepisani <61539880+irenepisani@users.noreply.github.com>
2021-03-17 17:20:58 +0100

    Update convertnewformat.py

--- Manual semantic annotation/convertnewformat.py
+++ Manual semantic annotation/convertnewformat.py
@@ -1,24 +1,25 @@
-#system for delete not useful information for alignment with format of the final dataset
+#delete useless information to make files compatible with the final dataset
+
+#INPUT: dataset_50_IP.conllu
+#OUTPUT: dataset_50_IP_new.conllu
+
+#INPUT: dataset_50_FP.conllu
+#OUTPUT: datset_50_FP_new.conllu
+
 import sys
 
 def main(file):
 
-	delete = [4,5,6,7,8,9]
+	delete = [4,5,6,7,8,9] #celle da svuotare
 
-	with open(file, 'r') as infile, open("dataset_50_FP_new.conllu", 'w') as outfile:
-		
+	with open(file, 'r') as infile, open("file.conllu", 'w') as outfile:
 		lines = infile.readlines()
 		for line in zip(lines):
 			linea = line[0]
-			#linea = list(line)
-			#print(linea)
 			linea = linea.split("\t")
-			print(linea)
-
 			for i in range(len(linea)):
 				if i in delete:
-					linea[i]="_"
-					
+					linea[i]="_"	
 			stampa=""
 			print(linea)
 			if len(linea)==10:
@@ -30,6 +31,4 @@ def main(file):
 					stampa= stampa+element+"\t"
 				stampa = stampa[0:-1]
 			outfile.writelines(stampa)
-
-
 main(sys.argv[1])
