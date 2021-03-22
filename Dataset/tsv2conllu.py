268d67d442d2b42bd44bee3ac3eef01fee987cc4
irenepisani <61539880+irenepisani@users.noreply.github.com>
2021-03-22 11:36:24 +0100

    Add files via upload

---
+++ Dataset/tsv2conllu.py
@@ -0,0 +1,26 @@
+import sys
+import csv
+
+def get_conllufile (infile):
+	with open('dataset.conllu', 'w') as outfile:
+		for line in infile:
+			outfile.writelines(line)
+
+def main(file):
+
+	tsv_file = open(file)
+	read_tsv = csv.reader(tsv_file, delimiter="\t")
+	
+	with open('datasetFP.conllu', 'w') as outfile:
+		for line in read_tsv:
+			
+			a=['', '', '', '', '', '', '', '', '', '', '', '']
+			if line!= a:
+				stampa = ""
+				for element in line:
+					if element!=str(''):
+						stampa = stampa+str(element)+"\t"
+				stampa = stampa[0:-1]+"\n"
+				outfile.writelines(stampa)
+
+main(sys.argv[1])
